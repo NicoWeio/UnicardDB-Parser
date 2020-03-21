a7adf45027fe0894899128aeb6d8c74d5206977b
Nicolai Weitkemper <nico.weio@gmail.com>
2020-03-21 22:29:18 +0100

    mehr Zeichen

--- parser.py
+++ parser.py
@@ -11,10 +11,17 @@ while byte:
     # Do stuff with byte.
     byte = f.read(1)
     # print(byte)
-    # if byte == 0x84:
-    if byte == b'\x81':
+    if byte == b'\x84': #ä
+        byte = b'\xE4'
+    if byte == b'\x94': #ö
+        byte = b'\xF6'
+    if byte == b'\x81': #ü
         byte = b'\xFC'
-    if byte == b'\x1A':
+    if byte == b'\x9A': #Ü
+        byte = b'\xDC'
+    if byte == b'\xFA': #.
+        byte = b'\x2E'
+    if byte == b'\x1A': #Lücke
         continue
         # print("HIT!")
         # print(byte.decode("ISO-8859-1"), end = '')
