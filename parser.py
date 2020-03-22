33edd13ed17eff373dbffb1afdb99d5284996389
Nicolai Weitkemper <nico.weio@gmail.com>
2020-03-22 17:40:52 +0100

    …

--- parser.py
+++ parser.py
@@ -4,13 +4,16 @@
 # newFileByteArray = bytearray(newFileBytes)
 # newFile = open("EIGBUCH-edit.txt", "wb")
 
+entries = list()
+currentEntry = ""
+
 flag_newentry = False
 
-o = open("EIGBUCH-out-v1.txt", "w")
+# o = open("EIGBUCH-out-v1.txt", "w")
 
 f = open("EIGBUCH-head.dat", 'rb')
 # f = open("EIGBUCH-head.dat", 'rb')
-byte = f.read(1)
+byte = f.read(62)
 while byte:
     # Do stuff with byte.
     byte = f.read(1)
@@ -32,8 +35,30 @@ while byte:
     c = byte.decode("ISO-8859-1")
 
     if flag_newentry:
-        o.write("\n\n-----\n\n")
-        flag_newentry = False
-    o.write(c)
+        # o.write("\n\n-----\n\n")
 
-    print(c, end='')
+        entries.append(currentEntry)
+        currentEntry = ""
+
+        flag_newentry = False
+
+    currentEntry += c
+    # o.write(c)
+
+    # print(c, end='')
+
+for entry in entries:
+    # print(entry)
+    # print(entry.index('ISBN:'))
+    titleF = entry[:entry.index('ISBN:')]
+    # print(titleF)
+    rest = entry[entry.index('ISBN:'):]
+    # print(rest)
+    print(rest[:26])
+    print(rest[26:52])
+    print(rest[52:69])
+    print(rest[69:75])
+    print(rest[77:127])
+    # print(rest[:26])
+    # print(entry[90:95])
+    print('---')
