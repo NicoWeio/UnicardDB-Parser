74f9fbda2362f96c474db676f48df7f3aff06a15
Nicolai Weitkemper <nico.weio@gmail.com>
2020-03-24 00:06:37 +0100

    line-by-line parsing

--- parser.py
+++ parser.py
@@ -22,27 +22,28 @@ def read_single(content, startsWith):
 
 def read_v1(rest):
     entry = {
-        'isbn': read_single(rest[0:26],    'ISBN')[2:],
-        'verlag': read_single(rest[26:52],   'Verlag')[2:],
-        'sprache': read_single(rest[52:69],   'Sprache'),
-        'art': read_single(rest[69:75],   'Art'),
-        'originaltitel': read_single(rest[77:127],  'Originaltitel'),
-        'erstersch': read_single(rest[129:145], 'Erstersch.'),
-        'geliehen': read_single(rest[146:180], 'geliehen.von'),
-        'geliehen_am': read_single(rest[180:198], 'am'),
-        'ausleihe': read_single(rest[211:237], 'Ausleihe.offen?'),
-        'rueckg_datum': read_single(rest[237:263], 'Rückgdatum'),
-        'herkunft': read_single(rest[276:301], 'Herkunft'),
-        'herkunft_datum': read_single(rest[301:330], 'Herk.datum'),
-        'preis': read_single(rest[330:340], 'Preis'),
-        'verliehen': read_single(rest[341:365], 'verliehen'),
-        'verliehen_an': read_single(rest[366:397], 'an'),
+        'isbn': read_single(rest[0:26],    'ISBN:'),
+        'verlag': read_single(rest[26:52],   'Verlag:'),
+        'sprache': read_single(rest[52:69],   'Sprache:'),
+        'art': read_single(rest[69:75],   'Art:'),
+        'originaltitel': read_single(rest[77:127],  'Originaltitel:'),
+        'erstersch': read_single(rest[129:145], 'Erstersch.:'),
+        'geliehen': read_single(rest[146:180], 'geliehen.von:'),
+        'geliehen_am': read_single(rest[180:198], 'am:'),
+        'ausleihe': read_single(rest[211:237], 'Ausleihe.offen?:'),
+        'rueckg_datum': read_single(rest[237:263], 'Rückgdatum:'),
+        'herkunft': read_single(rest[276:301], 'Herkunft:'),
+        'herkunft_datum': read_single(rest[301:330], 'Herk.datum:'),
+        'preis': read_single(rest[330:340], 'Preis:'),
+        'verliehen': read_single(rest[340:365], 'verliehen..ja(')[:-1],
+        'verliehen_an': read_single(rest[365:397], 'an:'),
     }
     # read_single(rest[407:442], 'todo', 'Bemerkungen.und.Stichworte')
     # read_single(rest[444:460], 'todo', 'zu.lesen')
     # read_single(rest[460:493], 'todo', 'wo.gesehen')
     # read_single(rest[493:506], 'todo', 'Signatur')
     return entry
+
 
 def write_csv(entries):
     import csv
@@ -57,6 +58,7 @@ def write_csv(entries):
         for row in entries:
             writer.writerow(row)
 
+
 entries = list()
 currentEntry = ""
 
@@ -64,6 +66,7 @@ flag_newentry = False
 error_count = 0
 type1_count = 0
 type2_count = 0
+unrecognized_count = 0
 
 # o = open("EIGBUCH-out-v1.txt", "w")
 
@@ -107,41 +110,53 @@ parsed_entries = list()
 
 for entry in entries:
     # print(entry)
-    print("-----")
+    # print("-----")
 
     if 'verliehen? (n) an:' in entry:
-        print('Guessed Type 2: TODO')
+        # print('Guessed Type 2: TODO')
         type2_count += 1
         continue
     if not 'ISBN:' in entry:
         print('!!! quite fatal: ISBN not found')
         continue
-    if 'verliehen..ja(n)' in entry:
-        print('Guessed Type 1… All good… Yet…')
+    if 'verliehen..ja(' in entry:
+        # print('Guessed Type 1… All good… Yet…')
         type1_count += 1
     else:
         print('!!!!!!!!!!!!!!!!!!')
         print("Not recognized: ")
         print(entry)
         print('!!!!!!!!!!!!!!!!!!')
+        unrecognized_count += 1
 
     rest = entry[entry.index('ISBN:'):]
-    parsed_entry = read_v1(rest)
+    # parsed_entry = read_v1(rest)
+    # parsed_entries.append(parsed_entry)
 
+    lines = rest.splitlines()
+    parsed_entry = {
+        'isbn': read_single(lines[0][:26],    'ISBN:'),
+        'verlag': read_single(lines[0][26:52],   'Verlag:'),
+        'sprache': read_single(lines[0][52:69],   'Sprache:'),
+        'art': read_single(lines[0][69:75],   'Art:'),
+        'originaltitel': read_single(lines[1][:52],  'Originaltitel:'),
+        'erstersch': read_single(lines[1][52:], 'Erstersch.:'),
+        'geliehen': read_single(lines[3][:34], 'geliehen.von:'),
+        'geliehen_am': read_single(lines[3][34:52], 'am:'),
+        'ausleihe': read_single(lines[4][:26], 'Ausleihe.offen?:'),
+        'rueckg_datum': read_single(lines[4][26:52], 'Rückgdatum:'),
+        'herkunft': read_single(lines[7][:25], 'Herkunft:'),
+        'herkunft_datum': read_single(lines[7][25:54], 'Herk.datum:'),
+        'preis': read_single(lines[7][54:], 'Preis:'),
+        'verliehen': read_single(lines[8][:25], 'verliehen..ja(')[:-1],
+        'verliehen_an': read_single(lines[8][25:57], 'an:'),
+    }
     parsed_entries.append(parsed_entry)
 
-    # print(entry.index('ISBN:'))
-    # titleF = entry[:entry.index('ISBN:')]
-    # print(titleF.replace('\n', ' ').replace('\r', '').strip()[2:] + '\n\n')
-    # rest = entry[entry.index('ISBN:'):]
-    # # print(rest)
-    #
-    # result1 = read_v1(rest)
-    #
-    # print('\n---\n')
 
 print("ERROR COUNT: " + str(error_count))
 print("TYPE 1 COUNT: " + str(type1_count))
 print("TYPE 2 COUNT: " + str(type2_count))
+print("UNRECOGNIZED COUNT: " + str(unrecognized_count))
 
 write_csv(parsed_entries)
